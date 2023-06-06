43f39e96ec12e2a526df41019d5dbc5ef64dc671
Rafael <menezes.carloseng@gmail.com>
2023-06-06 09:25:47 -0300

    Create vistor registration form page, navigation to page registration in home page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:visitants/features/visitor_registration/presentation/pages/visitor_registration_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -21,7 +22,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(widget.title)],
+          children: [Text("Home")],
         ),
       ),
       body: Padding(
@@ -36,12 +37,18 @@ class _HomePageState extends State<HomePage> {
                 Text(formattedDateTime),
               ],
             ),
-            const SizedBox(height: 16),
+             SizedBox(height: 100),
             Container(
               height: 40,
               child: Expanded(
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => VisitorRegistrationPage()),
+                    );
+                  },
                   child: Text("Cadastrar visitante"),
                 ),
               ),
@@ -62,7 +69,7 @@ class _HomePageState extends State<HomePage> {
               child: Expanded(
                 child: ElevatedButton(
                   onPressed: () {},
-                  child: Text("Cadastrar visitante"),
+                  child: Text("Ligar para morador"),
                 ),
               ),
             ),
