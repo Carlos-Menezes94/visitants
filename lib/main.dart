894ee16e02c040a12e3e2a819c7ce9448bf8137d
Rafael <menezes.carloseng@gmail.com>
2023-06-08 14:10:37 -0300

    Ajust main and call ModuleApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:visitants/app/features/home/presentation/home_module.dart';
+
+import 'package:visitants/module/module_app.dart';
 
 import 'app/depedency_injector/depedency_injector.dart';
+import 'app/features/login/presentation/login_module.dart';
 import 'app/features/login/presentation/pages/login_page.dart';
 import 'core/firebase_options.dart';
 
@@ -11,13 +15,23 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  DepedencyInjector.start();
+  // DepedencyInjector.start();
 
-  runApp(const MyApp());
+  runApp(ModuleApp(
+    mainModule: LoginModule(),
+    childModules: [
+      HomeModule(),
+    ],
+    app: (home) => MyApp(home: home),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    Key? key,
+    required this.home,
+  }) : super(key: key);
+  final Widget home;
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +45,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: home,
     );
   }
 }
