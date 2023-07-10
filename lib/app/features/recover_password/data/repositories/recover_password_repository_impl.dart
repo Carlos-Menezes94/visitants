98ab17c2d657ca9e213fb6a3261771ef6565ec1a
Rafael <menezes.carloseng@gmail.com>
2023-07-10 08:40:51 -0300

    Created repositories abstract and implementation to recover password

---
+++ lib/app/features/recover_password/data/repositories/recover_password_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'package:dartz/dartz.dart';
+import 'package:visitants/app/features/recover_password/domain/repositories/recover_password_repository_abstract.dart';
+
+import '../../../../../core/failure.dart';
+import '../datasources/recover_password_data_source_impl.dart';
+import '../failures/error_recover_password_failure.dart';
+import '../failures/internal_network_failure.dart';
+import '../failures/invalid_email_failure.dart.dart';
+import '../failures/network_request_failed_failure.dart';
+import '../failures/too_many_requests_failure.dart';
+import '../failures/user_not_found_failure.dart';
+
+class RecoverPasswordRepositoryImpl
+    implements RecoverPasswordRepositoryAbstract {
+  final RecoverPasswordDataSourceImpl recoverPasswordDataSourceImpl;
+
+  RecoverPasswordRepositoryImpl({required this.recoverPasswordDataSourceImpl});
+  @override
+  Future<Either<Failure, dynamic>> recoverPasswordRepository(
+      {required String email}) async {
+    try {
+      final response =
+          await recoverPasswordDataSourceImpl.recoverPassword(email: email);
+      if (response.success != true) {
+        switch (response.data) {
+          case "user-not-found":
+            return Left(UserNotFoundFailure());
+          case "invalid-email":
+            return Left(InvalidEmailFailure());
+          case "too-many-requests":
+            return Left(TooManyRequestsFailure());
+          case "network-request-failed":
+            return Left(NetworkRequestFailedFailure());
+          case "internal-error":
+            return Left(InternalNetworkFailure());
+          default:
+            return Left(ErrorRecoverPasswordFailure());
+        }
+      } else {
+        return const Right(true);
+      }
+    } catch (error) {
+      return Left(ErrorRecoverPasswordFailure());
+    }
+  }
+}
