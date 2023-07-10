e5f7604c07f9d638e56c234db11e38929872360a
Rafael <menezes.carloseng@gmail.com>
2023-07-10 08:49:11 -0300

    Created the controller method that starts the password recovery flow and injects the classes into the injector

--- lib/app/features/recover_password/presentation/controllers/recover_password_controller.dart
+++ lib/app/features/recover_password/presentation/controllers/recover_password_controller.dart
@@ -1,3 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:visitants/core/controller.dart';
 
-class RecoverPasswordController extends Controller {}
+import '../../../../../utils/toast.dart';
+import '../../domain/usecases/recover_password_usecase.dart';
+
+class RecoverPasswordController extends Controller {
+  final RecoverPasswordUseCase usecase;
+
+  RecoverPasswordController({required this.usecase});
+
+  Future<void> sendLinkRecoverPassword(BuildContext context) async {
+    final response = await usecase.recoverPasswordUseCase(
+        email: "menezes.carloseng@gmail.com");
+
+    response.fold((failure) {
+      print(failure.message);
+      return ToastHandler().showMyCustomToast(
+        context,
+        backgroundColor: Colors.yellowAccent.shade400,
+        color: Colors.black,
+        text: failure.message,
+      );
+    }, (sucessSendLinkToRecoverPassword) {
+      return sucessSendLinkToRecoverPassword;
+    });
+  }
+}
--- lib/app/features/recover_password/presentation/recover_password_injector.dart
+++ lib/app/features/recover_password/presentation/recover_password_injector.dart
@@ -2,23 +2,33 @@ import 'package:visitants/app/features/recover_password/presentation/stores/reco
 
 import '../../../../module/module_injector.dart';
 
+import '../data/datasources/recover_password_data_source_impl.dart';
+import '../data/repositories/recover_password_repository_impl.dart';
+import '../domain/usecases/recover_password_usecase.dart';
 import 'controllers/recover_password_controller.dart';
 import 'recover_password_module.dart';
 
 class RecoverPasswordInjector extends ModuleInjector<RecoverPasswordModule> {
   @override
   void controllers() {
-    registerFactory(() => RecoverPasswordController());
+    registerFactory(() => RecoverPasswordController(
+          usecase: get<RecoverPasswordUseCase>(),
+        ));
   }
 
   @override
   void core() {}
 
   @override
-  void datasources() {}
+  void datasources() {
+    registerFactory(() => RecoverPasswordDataSourceImpl());
+  }
 
   @override
-  void repositories() {}
+  void repositories() {
+    registerFactory(() => RecoverPasswordRepositoryImpl(
+        recoverPasswordDataSourceImpl: get<RecoverPasswordDataSourceImpl>()));
+  }
 
   @override
   void stores() {
@@ -26,5 +36,8 @@ class RecoverPasswordInjector extends ModuleInjector<RecoverPasswordModule> {
   }
 
   @override
-  void usecases() {}
+  void usecases() {
+    registerFactory(() => RecoverPasswordUseCase(
+        repository: get<RecoverPasswordRepositoryImpl>()));
+  }
 }
