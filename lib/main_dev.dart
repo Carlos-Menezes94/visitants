423c908bb1bf36778f2d2d1e1581f14a822513e2
Rafael <menezes.carloseng@gmail.com>
2023-07-14 21:44:57 -0300

    Configured sentry in homolog and prod environment

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,11 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:visitants/app/features/home/presentation/home_module.dart';
-import 'package:visitants/module/module_app.dart';
+
+import 'app/features/home/presentation/home_module.dart';
 import 'app/features/login/presentation/login_module.dart';
 import 'app/features/recover_password/presentation/recover_password_module.dart';
 import 'core/firebase_options.dart';
+import 'module/module_app.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +24,27 @@ void main() async {
     app: (home) => MyApp(home: home),
   ));
 }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   await SentryFlutter.init(
+//     (options) {
+//       // options.dsn =
+//       //     'https://8d0e5e6aa04d4f929f297e8eff576675@o4505518017150976.ingest.sentry.io/4505518018461696';
+
+//       // options.tracesSampleRate = 1.0;
+//     },
+//     appRunner: () => runApp(ModuleApp(
+//       mainModule: LoginModule(),
+//       childModules: [
+//         HomeModule(),
+//         RecoverPasswordModule(),
+//       ],
+//       app: (home) => MyApp(home: home),
+//     )),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({
