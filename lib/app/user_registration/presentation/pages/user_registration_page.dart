cec8b6fd6261c7e2df982f455a9157dde97a02a7
Rafael <menezes.carloseng@gmail.com>
2023-07-26 13:47:25 -0300

    Created state sucess view user registration

--- lib/app/user_registration/presentation/pages/user_registration_page.dart
+++ lib/app/user_registration/presentation/pages/user_registration_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:visitants/app/login/presentation/login_module.dart';
 import 'package:visitants/core/state.dart';
 
+import '../../../../core/asset_loader.dart';
 import '../controllers/user_registration_controller.dart';
 import '../stores/user_registration_store.dart';
 import '../user_registration_module.dart';
@@ -21,60 +23,79 @@ class UserRegistrationPageState extends StatePage<UserRegistrationModule,
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
-      controller.createUserNew();
+      controller.createUserNew(context);
       // controller.registerNewVisitor(context);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cadastro de novo usuário'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: LoginModule.to.navigator.pop,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: controller.store.email,
-                decoration: const InputDecoration(labelText: 'E-mail'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return '[Por favor, insira um e-mail]';
-                  }
-                  return null;
-                },
+    return ValueListenableBuilder(
+        valueListenable: store.state,
+        builder: (context, state, child) {
+          if (state.hasSuccess()) {
+            return Scaffold(
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Lottie.asset(
+                      AssetLoader.sucessLottie,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
               ),
-              TextFormField(
-                // inputFormatters: [CPFMask.inputMask],
-                controller: controller.store.password,
-                decoration: const InputDecoration(labelText: 'Senha'),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return '[Por favor, insira uma senha]';
-                  }
-                  return null;
-                },
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Cadastro de novo usuário'),
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: LoginModule.to.navigator.pop,
               ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: const Text(
-                  'Enviar',
-                  style: TextStyle(color: Colors.black),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: controller.store.email,
+                      decoration: const InputDecoration(labelText: 'E-mail'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return '[Por favor, insira um e-mail]';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      // inputFormatters: [CPFMask.inputMask],
+                      controller: controller.store.password,
+                      decoration: const InputDecoration(labelText: 'Senha'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return '[Por favor, insira uma senha]';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 40),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      child: const Text(
+                        'Enviar',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
